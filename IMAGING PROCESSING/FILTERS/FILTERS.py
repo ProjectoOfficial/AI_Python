1e553cacc9313feee0c63ee4bc9f1bf80fc598fe
ProjectoOfficial <miniprojectsofficial@gmail.com>
2022-03-26 15:23:21 +0100

    added SOBEL

--- IMAGING PROCESSING/FILTERS/FILTERS.py
+++ IMAGING PROCESSING/FILTERS/FILTERS.py
@@ -1,12 +1,13 @@
-# @author Dott. Daniel Rossi
-
 import torch
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import torchvision
+from torch.nn.functional import conv2d
+import cv2
+from scipy import ndimage
 
-PATH = r"C:\Users\daniel\Documents\GitHub Repositories\AI_Python\IMAGING PROCESSING\FILTERS\gatto.jpg"
+PATH = r"C:\Users\daniel\Desktop\Filtri\gatto.jpg"
 
 def load_image(infilename):
     """This function loads an image into memory when you give it
@@ -19,113 +20,122 @@ def load_image(infilename):
     data = np.asarray(img, dtype="float32")
     return data
 
-def linear_stretch(image: torch.tensor, a: torch.float32, b: torch.float32, device: torch.device):
-    return torch.clamp(torch.round((image.to(torch.float32) * a) + b).to(device), min=0, max=255).to(device).type(torch.uint8)
+def linear_stretch(image: torch.tensor, a: torch.float32, b: torch.float32):
+    return torch.clamp(torch.round((image.to(torch.float32) * a) + b), min=0, max=255).type(torch.uint8)
 
 
 def otsu_treshold(image: torch.tensor, device: torch.device):
     hist = torch.zeros((256, ))
     for i in range(256):
-        hist[i] = torch.sum(image.type(torch.float32) == i).to(device)
+        hist[i] = torch.sum(image.type(torch.float32) == i)
 
     tresh = -1
     maxvar = -1
     dat = torch.arange(256).type(torch.float32)
 
     for i in range(256):
-        w1 = hist[:i].sum().to(device)
-        w2 = hist[i:].sum().to(device)
+        w1 = hist[:i].sum()
+        w2 = hist[i:].sum()
 
         if w1 == 0 or w2 == 0:
             continue
-        m1 = torch.sum(hist[:i]* dat[:i]).to(device)/w1
-        m2 = torch.sum(hist[i:]* dat[i:]).to(device)/w2
+        m1 = torch.sum(hist[:i]* dat[:i])/w1
+        m2 = torch.sum(hist[i:]* dat[i:])/w2
         var = w1*w2*((m1-m2)**2)
         if var > maxvar:
             maxvar = var
             tresh = i
-
-    if tresh > 256:
-        return 255
-    
-    if tresh <=0:
-        return 0
-
     return tresh - 1
 
 
-def median_treshold(image: torch.tensor, device: torch.device):
-    return torch.median(torch.median(image).to(device)).to(device).to(torch.uint8)
+def tresh_filter(image:torch.tensor, treshold: torch.uint8):
+    print("Using treshold: {}".format(treshold))
+    return ((image > treshold) * 255).type(torch.uint8)
 
 
-def average_treshold(image: torch.tensor, device: torch.device):
-    image = image.to(torch.float32)
-    return torch.mean(torch.mean(image).to(device)).to(device).to(torch.uint8)
-
-def minmax_treshold(image: torch.tensor, device: torch.device):
-    min = torch.min(torch.min(image).to(device)).to(device)
-    max = torch.max(torch.max(image).to(device)).to(device)
-    return (torch.round((max - min) / 2).to(device)).to(torch.uint8)
- 
-def tresh_filter(image:torch.tensor, treshold: torch.uint8, device: torch.device, label: str):
-    print("{}: {}".format(label, treshold))
-    return ((image > treshold).to(device) * 255).type(torch.uint8)
-
-def im_show(image: torch.tensor, label: str):
-    image = image.detach().cpu()
+def im_show(image: torch.tensor):
     plt.imshow(image, cmap="gray")
-    plt.title(label)
     plt.show()
 
-if __name__ == "__main__":
-    if torch.cuda.is_available():
-        device = "cuda"
-        print("Actual GPU in use: {}".format(torch.cuda.get_device_name(device)))
-    else:
-        device = "cpu"
-    print("Running on device: {}".format(device))
 
+def run_prev_filters():
     im = load_image(PATH)
     im = torch.from_numpy(im).type(torch.uint8)
-   
-    im = im.to(device)
-    
-    im_show(im, "Original image")
 
     # linear stretch
-    filter_name = "Linear stretch"
-    filtered = linear_stretch(im, 1.2, 25, device)
-    im_show(filtered, filter_name)
+    filtered = linear_stretch(im, 0.8, 5)
+    im_show(filtered)
 
     # casual treshold filter
-    filter_name = "Casual treshold"
     treshold = 70
-    filtered = tresh_filter(im, treshold, device, filter_name)
-    im_show(filtered, filter_name)
-
-    # Average Filter
-    filter_name = "Average treshold"
-    treshold = average_treshold(im, device)
-    filtered = tresh_filter(im, treshold, device, filter_name)
-    im_show(filtered, filter_name)
-
-    # Median Filter
-    filter_name = "Median treshold"
-    treshold = median_treshold(im, device)
-    filtered = tresh_filter(im, treshold, device, filter_name)
-    im_show(filtered, filter_name)
-    
-    # MinMax Filter
-    filter_name = "MinMax treshold"
-    treshold = minmax_treshold(im, device)
-    filtered = tresh_filter(im, treshold, device, filter_name)
-    im_show(filtered, filter_name)
+    filtered = tresh_filter(im, treshold)
+    im_show(filtered)
 
     # OTSU treshold filter
-    filter_name = "OTSU treshold"
     treshold = otsu_treshold(im, device)
-    filtered = tresh_filter(im, treshold, device, filter_name)
-    im_show(filtered, filter_name)
+    filtered = tresh_filter(im, treshold)
+    im_show(filtered)
+
+
+def sobel(image:torch.Tensor, device: torch.device):
+    im = image.to(torch.float32).to(device)
+    Sx = torch.tensor([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=torch.float32).reshape((1,1,3,3)).to(device)
+    Sy = torch.tensor([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], dtype=torch.float32).reshape((1,1,3,3)).to(device)
+
+    gx = conv2d(im.reshape((1, 1, im.shape[0], im.shape[1])), Sx).to(device)
+    gy = conv2d(im.reshape((1, 1, im.shape[0], im.shape[1])), Sy).to(device)
+
+    m = ((gx**2 + gy**2)**(0.5)) / 1140 *255
+    d = (torch.atan2(gy, gx)*90/np.pi) + 90
+    s = torch.ones(m.shape).to(torch.float32) * 100
+    
+    image = torch.cat((d,s,m), dim=0).squeeze(1)
+    image = cv2.cvtColor(image.numpy().swapaxes(0, 2).swapaxes(0, 1).astype(np.uint8), cv2.COLOR_HSV2RGB)
+    return image
+
+
+def live_filtering(filter, params: tuple):
+    cam = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cam.read()
+        frame = np.asarray(frame.mean(axis=2), dtype="float32")
+
+        frame = torch.from_numpy(frame).type(torch.uint8)
+        #result = filter(frame, params[0], params[1]).numpy()   # Linear Stretch
+        #result = filter(frame, params[0]).numpy()              # Casual treshold
+
+        #t = otsu_treshold(frame, params[0])
+        #result = filter(frame, t).numpy()                      # OTSU
+
+        result = filter(frame, params[0])                       # SOBEL
+        
+        cv2.imshow("Live Filter", result)
+
+        k = cv2.waitKey(1)
+        if k%256 == 27:
+            # ESC pressed
+            print("ESC pressed, closing...")
+            break
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Running on {}".format(device))
+    if torch.cuda.is_available():
+        print("Actual GPU in use: {}".format(torch.cuda.get_device_name(device))) 
+
+    # run_prev_filters()
+
+    # live_filtering(linear_stretch, (0.8, 10, ))   # LINEAR TRESHOLD LIVE
+    # live_filtering(tresh_filter, (30, ))          # CASUAL TRESHOLD LIVE
+    # live_filtering(tresh_filter, (device, ))      # OTSU LIVE
+    live_filtering(sobel, (torch.device("cpu"), ))               # SOBEL LIVE
+
+    
+    
 
 
 
